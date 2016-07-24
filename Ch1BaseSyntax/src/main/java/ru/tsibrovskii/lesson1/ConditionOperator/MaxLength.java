794d627f9e5efacd924db2dd027ff1b1ee422d4f
Tsibrovskii <tsibrovskii@gmail.com>
2016-07-24 23:09:47 +0300

    length method

---
+++ Ch1BaseSyntax/src/main/java/ru/tsibrovskii/lesson1/ConditionOperator/MaxLength.java
@@ -0,0 +1,19 @@
+package ru.tsibrovskii.lesson1.ConditionOperator;
+
+public class MaxLength {
+	
+	double max;
+	
+	public double findOfMax(double dist1, double dist2, double dist3) {
+		
+		if (dist1 > dist2)
+			max = dist1;
+		else
+			max = dist2;
+		
+		if (max < dist3)
+			max = dist3;
+		
+		return max;		
+	}
+}
