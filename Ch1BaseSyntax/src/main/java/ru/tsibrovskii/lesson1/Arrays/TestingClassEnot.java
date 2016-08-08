ad1df6ff4256f1a1a1be45267a64ad49c3010b9a
Aleksei <tsibrovskii@gmail.com>
2016-08-08 14:05:50 +0300

    testenot

---
+++ Ch1BaseSyntax/src/main/java/ru/tsibrovskii/lesson1/Arrays/TestingClassEnot.java
@@ -0,0 +1,10 @@
+package ru.tsibrovskii.lesson1.Arrays;
+
+/**
+ * Created by Tide on 08.08.2016.
+ */
+public class TestingClassEnot {
+    public static void main(String[] args) {
+        System.out.println("asf");
+    }
+}
