a837f1fcf88bc1eb972402c405b424b336b0c2ca
Aleksei <tsibrovskii@gmail.com>
2017-07-24 23:55:11 +0300

    CollectionSpeed

---
+++ Ch5.0.CollectionsLite/src/main/java/ru/tsibrovskii/CollectionsFramework/CollectionsSpeed.java
@@ -0,0 +1,62 @@
+package ru.tsibrovskii.CollectionsFramework;
+
+import java.util.*;
+
+/**
+ * Класс тестирования скорости коллекций.
+ */
+public class CollectionsSpeed {
+
+    /**
+     * Основной метод класса.
+     * @param args аргументы.
+     */
+    public static void main(String[] args) {
+
+        List<String> linkedList = new LinkedList<>();
+        List<String> arrayList = new ArrayList<>();
+        Set<String> treeSet = new TreeSet<>();
+
+        CollectionsSpeed collectionsSpeed = new CollectionsSpeed();
+
+        System.out.println(String.format("Время вставки в LinkedList: %s с", collectionsSpeed.add(linkedList, 50000)));
+        System.out.println(String.format("Время удаления из LinkedList: %s нс", collectionsSpeed.delete(linkedList, 10000)));
+
+        System.out.println(String.format("Время вставки в ArrayList: %s нс", collectionsSpeed.add(arrayList, 50000)));
+        System.out.println(String.format("Время удаления из ArrayList: %s нс", collectionsSpeed.delete(arrayList, 10000)));
+
+        System.out.println(String.format("Время вставки в TreeSet: %s нс", collectionsSpeed.add(treeSet, 50000)));
+        System.out.println(String.format("Время удаления из TreeSet: %s нс", collectionsSpeed.delete(treeSet, 10000)));
+    }
+
+    /**
+     * Метод вставки элементов в коллекцию.
+     * @param collection коллекция.
+     * @param amount количество элементов.
+     * @return время работы.
+     */
+    public long add(Collection<String> collection, int amount) {
+
+        long startTime = System.nanoTime();
+        for(int i = 0; i < amount; i++) {
+            collection.add(((Integer) i).toString());
+        }
+        return (System.nanoTime() - startTime);
+    }
+
+    /**
+     * Метод удаления элементов из коллекции.
+     * @param collection коллекция.
+     * @param amount количество элементов.
+     * @return время работы.
+     */
+    public long delete(Collection<String> collection, int amount) {
+
+        long startTime = System.nanoTime();
+        for(int i = 0; i < amount; i++) {
+            collection.remove(((Integer) (i*2)).toString());
+        }
+        return (System.nanoTime() - startTime);
+
+    }
+}
