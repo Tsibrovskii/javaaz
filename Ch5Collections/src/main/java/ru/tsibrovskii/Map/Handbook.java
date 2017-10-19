d96e93a6b81b2b4675a14a374aea6ffb03b3cbb4
Aleksei <tsibrovskii@gmail.com>
2017-10-19 23:52:00 +0300

    HandbookIterator

--- Ch5Collections/src/main/java/ru/tsibrovskii/Map/Handbook.java
+++ Ch5Collections/src/main/java/ru/tsibrovskii/Map/Handbook.java
@@ -1,14 +1,39 @@
 package ru.tsibrovskii.Map;
 
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
 /**
  * Класс справочника.
  */
 public class Handbook<T, V> {
 
-    private T[] keys = (T[]) new Object[16];
-    private V[] values = (V[]) new Object[this.keys.length];
+    /**
+     * Класс ячейки.
+     * @param <T> ключ.
+     * @param <V> значение.
+     */
+    protected class MapHandbook<T, V> {
+
+        private T key;
+        protected V value;
+
+        /**
+         * Конструктор класса.
+         * @param key ключ.
+         * @param value значение.
+         */
+        private MapHandbook(T key, V value) {
+            this.key = key;
+            this.value = value;
+        }
+    }
+
+    private MapHandbook[] mapArray = new MapHandbook[16];
 
     private int count = 0;
+    private int lastPosition = 0;
+    private int firstPosition = -1;
 
     /**
      * Метод вставки элемента в спровочник.
@@ -18,19 +43,36 @@ public class Handbook<T, V> {
      */
     public boolean insert(T key, V value) {
 
-        if(this.count == this.keys.length) {
-            this.increaseArray();
-        }
-
         boolean result = false;
 
-        int index = key.hashCode() & (this.keys.length - 1);
+        if(key != null) {
 
-        if(this.keys[index] == null) {
-            this.keys[index] = key;
-            this.values[index] = value;
-            this.count++;
-            result = true;
+            MapHandbook mHandbook = new MapHandbook(key, value);
+
+            if (this.count == this.mapArray.length) {
+                this.increaseArray();
+            }
+
+            int index = key.hashCode() & (this.mapArray.length - 1);
+
+            if (this.mapArray[index] == null) {
+
+                this.mapArray[index] = mHandbook;
+
+                this.count++;
+
+                if(index > this.lastPosition) {
+                    this.lastPosition = index;
+                }
+
+                if(this.firstPosition < 0) {
+                    this.firstPosition = index;
+                } else if (this.firstPosition > 0 && this.firstPosition > index) {
+                    this.firstPosition = index;
+                }
+
+                result = true;
+            }
         }
 
         return result;
@@ -42,7 +84,11 @@ public class Handbook<T, V> {
      * @return значение.
      */
     public V getElement(T key) {
-        return this.values[key.hashCode() & (this.keys.length - 1)];
+
+        if(this.mapArray[key.hashCode() & (this.mapArray.length - 1)] == null) {
+            throw new NoSuchElementException();
+        }
+        return (V) this.mapArray[key.hashCode() & (this.mapArray.length - 1)].value;
     }
 
     /**
@@ -54,13 +100,15 @@ public class Handbook<T, V> {
 
         boolean result = false;
 
-        int index = key.hashCode() & (this.keys.length - 1);
+        if(key != null) {
 
-        if (index < this.keys.length) {
-            this.keys[index] = null;
-            this.values[index] = null;
-            this.count--;
-            result = true;
+            int index = key.hashCode() & (this.mapArray.length - 1);
+
+            if (index < this.mapArray.length) {
+                this.mapArray[index] = null;
+                this.count--;
+                result = true;
+            }
         }
 
         return result;
@@ -70,13 +118,64 @@ public class Handbook<T, V> {
      * Метод расширения массива.
      */
     private void increaseArray() {
-        T[] bigKeys = (T[]) new Object[this.keys.length * 2];
-        V[] bigValues = (V[]) new OutOfMemoryError[this.keys.length * 2];
-        for(T t : this.keys) {
-            bigKeys[t.hashCode() & (bigKeys.length - 1)] = t;
-            bigValues[t.hashCode() & (bigKeys.length - 1)] = this.values[t.hashCode() & (this.keys.length - 1)];
+
+        this.lastPosition = 0;
+        this.firstPosition = -1;
+
+        MapHandbook[] bigMap = new MapHandbook[this.mapArray.length * 2];
+
+        for(MapHandbook mH : this.mapArray) {
+            int index = mH.key.hashCode() & (bigMap.length - 1);
+            bigMap[index] = mH;
+
+            if(index > this.lastPosition) {
+                this.lastPosition = index;
+            }
+
+            if(this.firstPosition < 0) {
+                this.firstPosition = index;
+            } else if (this.firstPosition > 0 && this.firstPosition > index) {
+                this.firstPosition = index;
+            }
         }
-        this.keys = bigKeys;
-        this.values = bigValues;
+        this.mapArray = bigMap;
+    }
+
+    /**
+     * Метод итератора.
+     * @return итератор.
+     */
+    public Iterator<MapHandbook> iterator() {
+        return new Iterator<MapHandbook>() {
+
+            private int position = firstPosition;
+
+            public boolean hasNext() {
+
+                boolean result = false;
+
+                if(this.position >= 0) {
+                    result = this.position <= lastPosition;
+                }
+                return result;
+            }
+
+            public MapHandbook next() {
+
+                MapHandbook result = null;
+
+                if (this.hasNext()) {
+                    while (mapArray[this.position] == null) {
+                        this.position++;
+                    }
+                    result = mapArray[this.position];
+                    this.position++;
+                } else {
+                    throw new NoSuchElementException();
+                }
+
+                return result;
+            }
+        };
     }
 }
--- Ch5Collections/src/test/java/ru/tsibrovskii/Map/HandbookTest.java
+++ Ch5Collections/src/test/java/ru/tsibrovskii/Map/HandbookTest.java
@@ -1,7 +1,101 @@
 package ru.tsibrovskii.Map;
 
+import org.junit.Test;
+
+import java.util.Iterator;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
+
 /**
  * Тестовый класс.
  */
 public class HandbookTest {
+
+    /**
+     * Тестовый метод.
+     */
+    @Test
+    public void whenInsertKeyAndValueShouldIncreaseMap() {
+
+        Handbook<String, Integer> handbook = new Handbook<>();
+
+        handbook.insert("key1", 1);
+        handbook.insert("key2", 2);
+
+        Integer result = handbook.getElement("key2");
+        assertThat(result, is(2));
+    }
+
+    /**
+     * Тестовый метод.
+     */
+    @Test
+    public void whenDeleteShouldDecreaseMap() {
+
+        Handbook<String, Integer> handbook = new Handbook<>();
+
+        handbook.insert("key1", 1);
+        handbook.insert("key2", 2);
+        handbook.insert("key1", 3);
+        handbook.delete("key2");
+
+        Integer result = handbook.getElement("key1");
+        assertThat(result, is(1));
+    }
+
+    /**
+     * Тестовый метод.
+     */
+    @Test
+    public void whenHasElementShouldReturnTrue() {
+
+        Handbook<String, Integer> handbook = new Handbook<>();
+
+        handbook.insert("key1", 1);
+        handbook.insert("key2", 2);
+
+        Iterator<Handbook.MapHandbook> iterator = handbook.iterator();
+
+        iterator.next();
+        assertTrue(iterator.hasNext());
+    }
+
+    /**
+     * Тестовый метод.
+     */
+    @Test
+    public void whenHasNoElementShouldReturnFalse() {
+
+        Handbook<String, Integer> handbook = new Handbook<>();
+
+        handbook.insert("key1", 1);
+        handbook.insert("key2", 2);
+
+        Iterator<Handbook.MapHandbook> iterator = handbook.iterator();
+
+        iterator.next();
+        iterator.next();
+        assertFalse(iterator.hasNext());
+    }
+
+    /**
+     * Тестовый метод.
+     */
+    @Test
+    public void whenRequireNextElementShouldReturnElement() {
+
+        Handbook<String, Integer> handbook = new Handbook<>();
+
+        handbook.insert("key1", 1);
+        handbook.insert("key2", 2);
+
+        Iterator<Handbook.MapHandbook> iterator = handbook.iterator();
+
+        iterator.next();
+        Handbook.MapHandbook mapHandbook = iterator.next();
+        assertThat((Integer) mapHandbook.value, is(2));
+    }
 }
