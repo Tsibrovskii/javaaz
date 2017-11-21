eec87f3d5dbb2b7f7e4cb685b38eb62484aeaf75
Aleksei <tsibrovskii@gmail.com>
2017-11-21 23:57:25 +0300

    TreeIterator

--- Ch5Collections/src/main/java/ru/tsibrovskii/Tree/Tree.java
+++ Ch5Collections/src/main/java/ru/tsibrovskii/Tree/Tree.java
@@ -1,8 +1,6 @@
 package ru.tsibrovskii.Tree;
 
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
+import java.util.*;
 
 /**
  * Класс дерева.
@@ -109,6 +107,127 @@ public class Tree<E extends Comparable<E>> implements SimpleTree<E> {
      */
     @Override
     public Iterator<E> iterator() {
-        return null;
+        return new Iterator<E>() {
+
+            boolean alreadyInit = false;
+            Node<E> parent;
+            E returnedElement;
+
+            /**
+             * Метод, проверяющий, есть ли еще элементы для возврата.
+             * @return результат.
+             */
+            @Override
+            public boolean hasNext() {
+                boolean isHasNext = false;
+                if(root.value != null) {
+                    if(returnedElement == null) {
+                        isHasNext = true;
+                    } else {
+                        isHasNext = !root.value.equals(returnedElement);
+                    }
+                }
+                return isHasNext;
+            }
+
+            /**
+             * Метод, возвращающий следующее значение.
+             * @return значение.
+             */
+            @Override
+            public E next() {
+
+                boolean success = false;
+
+                if(!hasNext()) {
+                    throw new NoSuchElementException();
+                }
+
+                if(returnedElement == null) {
+                    initReturnedElement(root);
+                    success = true;
+                }
+
+                if(!success && returnedElement.equals(parent.value)) {
+                    initParent(root);
+                    initReturnedElement(parent);
+                    success = true;
+                }
+
+                if(!success) {
+                    boolean successInArray = false;
+                    int index = -1;
+                    for(Node n : parent.children) {
+                        if(n.value.equals(returnedElement)) {
+                            index = parent.children.indexOf(n);
+                        }
+                    }
+                    if(parent.children.size() == index + 1) {
+                        returnedElement = parent.value;
+                        initParent(root);
+                        successInArray = true;
+                    }
+                    if(!successInArray && parent.children.get(index + 1).children == null) {
+                        returnedElement = parent.children.get(index + 1).value;
+                        successInArray = true;
+                    }
+                    if(!successInArray) {
+                        initReturnedElement(parent.children.get(index + 1));
+                    }
+                }
+
+                return returnedElement;
+            }
+
+            /**
+             * Метод определения следующего элемента для итератора.
+             * Первым элементом будет элемент, расположенный левее остальных.
+             * @param nodeElement начальный узел.
+             */
+            private void initReturnedElement(Node<E> nodeElement) {
+
+                returnedElement = nodeElement.children == null || nodeElement.children.isEmpty() ? nodeElement.value : null;
+
+                if(returnedElement == null) {
+                    for (Node n : nodeElement.children) {
+                        initReturnedElement(n);
+                        if(returnedElement != null) {
+                            if(!alreadyInit) {
+                                parent = nodeElement;
+                                alreadyInit = true;
+                            }
+                            break;
+                        }
+                    }
+                }
+            }
+
+            /**
+             * Метод определения родительского узла для дочернего элемента.
+             * @param nodeElement возможный родительский узла.
+             */
+            private void initParent(Node<E> nodeElement) {
+
+                if(nodeElement.children != null) {
+                    for(Node n : nodeElement.children) {
+                        if(n.value.equals(returnedElement)) {
+                            parent = nodeElement;
+                        }
+                    }
+
+                    if (parent == returnedElement) {
+                        for (Node n : nodeElement.children) {
+                            if (n != null) {
+                                initParent(n);
+                            }
+                            if (parent != returnedElement) {
+                                break;
+                            }
+                        }
+                    }
+                }
+
+            }
+        };
     }
 }
--- Ch5Collections/src/test/java/ru/tsibrovskii/Tree/TreeTest.java
+++ Ch5Collections/src/test/java/ru/tsibrovskii/Tree/TreeTest.java
@@ -3,13 +3,20 @@ package ru.tsibrovskii.Tree;
 import org.junit.Test;
 import ru.tsibrovskii.Tree.Tree.Node;
 
+import java.util.Iterator;
 import java.util.List;
+
+import static junit.framework.TestCase.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 /**
  * Тестовый класс.
  */
 public class TreeTest {
 
+    /**
+     * Тестовый метод.
+     */
     @Test
     public void whenAddElementShouldPastItInCollection() {
 
@@ -25,15 +32,95 @@ public class TreeTest {
         tree.add("second", "fifth");
         tree.add("second", "first");
 
-        System.out.println("root " + tree.root.value);
+        System.out.println("Order of iterator" + "\n");
         for (Node n : tree.root.children) {
-            System.out.println(n.value);
             if(n.children != null) {
                 for (Node m : (List<Node>)n.children) {
                     System.out.println(m.value);
                 }
             }
+            System.out.println(n.value);
         }
+        System.out.println(tree.root.value);
+    }
 
+    /**
+     * Тестовый метод.
+     */
+    @Test
+    public void whenCallHasNextShouldReturnTrueOrFalse() {
+
+        Tree<String> tree = new Tree<>();
+
+        tree.addFirst("first");
+
+        tree.add("first", "second");
+        tree.add("first", "third");
+        tree.add("first", "first");
+
+        tree.add("second", "fourth");
+        tree.add("second", "fifth");
+        tree.add("second", "first");
+
+        Iterator<String> it = tree.iterator();
+
+        boolean isHasNext = it.hasNext();
+        assertTrue(isHasNext);
+
+        it.next();
+        isHasNext = it.hasNext();
+        assertTrue(isHasNext);
+
+        it.next();
+        isHasNext = it.hasNext();
+        assertTrue(isHasNext);
+
+        it.next();
+        isHasNext = it.hasNext();
+        assertTrue(isHasNext);
+
+        it.next();
+        isHasNext = it.hasNext();
+        assertTrue(isHasNext);
+
+        it.next();
+        isHasNext = it.hasNext();
+        assertFalse(isHasNext);
+    }
+
+    /**
+     * Тестовый метод.
+     */
+    @Test
+    public void whenCallHasNextShouldReturnNextElement() {
+
+        Tree<String> tree = new Tree<>();
+
+        tree.addFirst("first");
+
+        tree.add("first", "second");
+        tree.add("first", "third");
+        tree.add("first", "first");
+
+        tree.add("second", "fourth");
+        tree.add("second", "fifth");
+        tree.add("second", "first");
+
+        Iterator<String> it = tree.iterator();
+
+        String next = it.next();
+        assertTrue("fourth".equals(next));
+
+        next = it.next();
+        assertTrue("fifth".equals(next));
+
+        next = it.next();
+        assertTrue("second".equals(next));
+
+        next = it.next();
+        assertTrue("third".equals(next));
+
+        next = it.next();
+        assertTrue("first".equals(next));
     }
 }
